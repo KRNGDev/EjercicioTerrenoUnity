d4813a3c1ae6107228afd51be5c548427309a50b
KRNGDev <161486725+KRNGDev@users.noreply.github.com>
2024-03-06 20:24:34 +0100

    update

--- Assets/GameAssets/Scripts/UI/UiManager.cs
+++ Assets/GameAssets/Scripts/UI/UiManager.cs
@@ -5,8 +5,55 @@ using UnityEngine;
 
 public class UiManager : MonoBehaviour
 {
+    public GameObject panelMenu;
+    public GameObject botonMenu;
+    public void ActivarPanel()
+    {
+        panelMenu.SetActive(true);
+        botonMenu.SetActive(false);
+    }
+    public void OcultarPanel()
+    {
+        panelMenu.SetActive(false);
+        botonMenu.SetActive(true);
+    }
+    public void EjecutarLoadScene()
+    {
+        StartCoroutine(EsperarLoadScene());
+    }
+    public void EjecutarMenuPrincipal()
+    {
+        StartCoroutine(EsperarMenuPrincipal());
+    }
+
+    public IEnumerator EsperarLoadScene()
+    {
+        // Esperar 2 segundos
+        yield return new WaitForSeconds(0.5f);
+
+        // Llamar a la función que deseas activar 
+        LoadScene();
+    }
+    IEnumerator EsperarMenuPrincipal()
+    {
+        // Esperar 2 segundos
+        yield return new WaitForSeconds(1);
+
+        // Llamar a la función que deseas activar 
+        MenuPrincipal();
+    }
+
     public void LoadScene()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Exterior");
+    }
+    public void MenuPrincipal()
+    {
+        SceneManager.LoadScene("Intro");
+
+    }
+    public void Salir()
+    {
+        Application.Quit();
     }
 }
