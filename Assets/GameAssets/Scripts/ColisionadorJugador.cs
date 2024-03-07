b94fd50fdc1bbe2b660947a8fcfa040f4ce8e905
KRNGDev <161486725+KRNGDev@users.noreply.github.com>
2024-03-07 18:18:58 +0100

    update

--- Assets/GameAssets/Scripts/ColisionadorJugador.cs
+++ Assets/GameAssets/Scripts/ColisionadorJugador.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,34 @@ using UnityEngine;
 public class ColisionadorJugador : MonoBehaviour
 {
     public GameObject boton;
+    public GameObject mensajeKey;
+    public GameObject consegirKey;
+    public Boolean key = false;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Puerta"))
+        if (other.CompareTag("Puerta") && key == true)
         {
             boton.SetActive(true);
         }
+        else if (other.CompareTag("Key"))
+        {
+            key = true;
+            consegirKey.SetActive(true);
+            Destroy(other.gameObject);
+        }
+        else
+        {
+            mensajeKey.SetActive(true);
+        }
     }
-    private void OnTriggerExit(Collider other) {
+    private void OnTriggerExit(Collider other)
+    {
         if (other.CompareTag("Puerta"))
         {
             boton.SetActive(false);
+            mensajeKey.SetActive(false);
         }
     }
+
 }
